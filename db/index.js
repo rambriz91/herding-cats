7331af7b0f1a2ccd458968a5404a1c6efaa289f6
rambriz91 <rambriz91@gmail.com>
2023-10-02 14:48:24 -0700

    improved query for viewEmployees(), it now queries the table according to specifications.

--- db/index.js
+++ db/index.js
@@ -74,7 +74,11 @@ function viewQuery(sql) {
 
 //view Query functions
 function viewEmployees() {
-    const sql = 'SELECT * FROM employee';
+    const sql = `SELECT e.id, e.first_name, e.last_name, r.title, r.salary, d.dept_name, CONCAT(m.first_name, ' ', m.last_name) AS Manager 
+    FROM employee e 
+    LEFT JOIN roles r ON e.role_id = r.id 
+    LEFT JOIN department d ON r.department_id = d.id 
+    LEFT JOIN employee m ON e.manager_id = m.id;`
     viewQuery(sql);
 };
 
